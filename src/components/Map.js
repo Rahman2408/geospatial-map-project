21e2a86f5e4ba68d66d03a11f0eb71568687dc94
Rahman Elsirafy <rahman2408@gmail.com>
2022-04-25 03:49:48 -0400

    Fixes icon image issue

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState }  from "react";
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
 import "../App.css";
 import Mapbox from "./MapboxLayerApi";
 import mapData from "../data/mapData.json"
 import "leaflet/dist/leaflet.css"
-import { FormControlLabel, Switch } from "@material-ui/core";
-
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import { defaultIcon } from "../icons/defaultIcons";
 
 const Map =() => {
 
@@ -23,7 +24,7 @@ const Map =() => {
 //   this.setState({ mapView: Mapbox.id.satellite})
 // }
   // Our States
-  const [state, setState] = React.useState({ status: true });
+  const [state, setState] = useState({ status: true });
 
   // Change State Function
   const handleChange = (event) => {
@@ -32,20 +33,20 @@ const Map =() => {
 
     return (
       <>
-      <h1 style={{textAlign: "center"}}>Reserved space for content above the map.</h1>
+        <h1 style={{ textAlign: "center" }}>Reserved space for content above the map.(DCI County 2018 SHP Shown)</h1>
         
         <div className="toggleView">
           <FormControlLabel
             control={
               <Switch
                 checked= {state.status}
-                // onClick= {() => this.setState({satView: true, mapView: Mapbox.id.satellite})}
-                onChange= {state.handleChange}
+                // onClick= {handleChange}
+                onChange= {handleChange}
                 color= "primary"
                 name="toggleView"
                 />
               }
-              label="Sattelite View"
+              label="Sattelite View Toggle (Non-functional / In Development)"
           />
         </div>
 
@@ -53,7 +54,7 @@ const Map =() => {
           <ToggleView label="Sattelite View" />
         </React.Fragment> */}
 
-      <MapContainer center={[25.7617, -80.1918]} zoom={8}>
+        <MapContainer center={[18.2208, -66.5901]} zoom={9}>
 
           <TileLayer 
               attribution={Mapbox.attribution}
@@ -66,9 +67,9 @@ const Map =() => {
 
           <GeoJSON data={mapData.features} />
 
-          <Marker position={[25.7617, -80.1918]}>
+          <Marker position={[18.2208, -66.5901]} icon={defaultIcon}>
               <Popup>
-                  Just using Miami's coordinates for now with a very broad zoom. <br /> Helps facilitate changes to where the GeoJson data exists rather than scrolling from London every time.
+                  Sample Popup
               </Popup>
           </Marker>
 
